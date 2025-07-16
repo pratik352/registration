09f23f1478e6b8234ad22645353cd491fd8aaf1e
hritikpatle <hritikpatle775@gmail.com>
2025-07-16 06:47:01 +0000

    d

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,6 @@ const router = express.Router();
 // v1 API routes
 router.use("/v1/employees", v1Routes.employees);
 router.use("/v1/demo", v1Routes.demo);
+router.use("/v1/aadhar", v1Routes.aadharRoutes);
 
 module.exports = router;
