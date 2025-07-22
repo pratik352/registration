2878d7e99ad831b35b9ce7ba1256db893c237c2f
Yugal41735 <yugal1434@gmail.com>
2025-07-22 17:13:18 +0000

    feat: implement employee routes with search functionality and authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const v1Routes = require("./v1");
-
 const router = express.Router();
 
-// v1 API routes
-router.use("/v1/employees", v1Routes.employees);
-router.use("/v1/demo", v1Routes.demo);
+const v1Routes = require("./v1");
+
+router.use('/v1', v1Routes);
 
 module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,14 @@
-module.exports = {
-    demo: require("./demo.route.js"),
-    employees: require("./employee.routes.js")
-};
+const express = require('express');
+const router = express.Router();
+
+//Routes
+const attendanceRoutes = require('./attendanceRoute');
+const demoRoute = require('./demo.route')
+const employeeRoutes = require('./EmployeeRoute')
+
+// Route definitions
+router.use('./attendance', attendanceRoutes);
+router.use('./demo', demoRoute);
+router.use('./employee', employeeRoutes)
+
+module.exports = router;
