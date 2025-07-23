2c31d14cdec5658316891f7ed7531ce134b3047b
Yugal41735 <yugal1434@gmail.com>
2025-07-23 07:13:06 +0000

    refactor: rename Emp_management model to Employees and update related service methods
    - Changed model name from Emp_management to Employees in schema.prisma.
    - Updated attendanceService.js to reflect the new model name in markAttendance and unmarkAttendance functions.
    - Modified employee.service.js to use the new Employees model in getEmployeeByEmployeeId, pagination, and filtering methods.
    - Adjusted seed.js to delete and create employees using the new Employees model.

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -1,31 +1,17 @@
 const { PrismaClient, AttendanceStatus } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-//without async-await
-// const markAttendance = (employee_id) => {
-//   return prisma.emp_management.update({
-//     where: { employee_id },
-//     data: { attendance_status: 'Present' }
-//   });
-// };
 
 const markAttendance = async (employee_id) => {
-  return await prisma.emp_management.update({
+  return await prisma.employees.update({
     where: { employee_id },
     data: { attendance_status: AttendanceStatus.Present }
   });
 };
 
-//without async-await
-// const unmarkAttendance = (employee_id) => {
-//   return prisma.emp_management.update({
-//     where: { employee_id },
-//     data: { attendance_status: 'Absent' }
-//   });
-// };
 
 const unmarkAttendance = async (employee_id) => {
-  return await prisma.emp_management.update({
+  return await prisma.employees.update({
     where: { employee_id },
     data: { attendance_status: AttendanceStatus.Absent }
   });
