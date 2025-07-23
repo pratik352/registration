5de197d7925f34e4f415e968f74a46873c4dbd8c
Yugal41735 <yugal1434@gmail.com>
2025-07-23 06:48:39 +0000

    feat: implement QR code generation and WhatsApp messaging functionality for employee invitations

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,12 +4,16 @@ const router = express.Router();
 //Routes
 const attendanceRoutes = require('./attendanceRoute');
 const demoRoute = require('./demo.route')
-const employeeRoutes = require('./EmployeeRoute')
+const employeeRoutes = require('./EmployeeRoute');
+const userRoutes = require('./user.route');
+const aadharRoutes = require('./aadharRoutes');
+
 
 // Route definitions
 router.use('/attendance', attendanceRoutes);
 router.use('/demo', demoRoute);
 router.use('/employee', employeeRoutes)
 router.use("/aadhar", aadharRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
